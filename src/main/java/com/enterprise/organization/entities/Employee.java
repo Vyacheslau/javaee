5738f8f618c3fd79c4a3be42ab257ebd045ef6ee
Vyacheslau <vyacheslaukarachun@gmail.com>
2013-02-26 20:44:55 +0300

    User class was added. Login Controller was edited.

--- src/main/java/com/enterprise/organization/entities/Employee.java
+++ src/main/java/com/enterprise/organization/entities/Employee.java
@@ -1,54 +1,62 @@
 package com.enterprise.organization.entities;
 
 public class Employee {
-	
+
 	private long emploeeID;
 	private long managerID;
-	private long departmentID;
-	private long passportID;
+	private Department department;
+	private Passport passport;
 	private String jobTittle;
 	private int salary;
-		
+
 	public Employee() {
 		super();
 	}
-	
+
 	public long getEmploeeID() {
 		return emploeeID;
 	}
+
 	public void setEmploeeID(long emploeeID) {
 		this.emploeeID = emploeeID;
 	}
+
 	public long getManagerID() {
 		return managerID;
 	}
+
 	public void setManagerID(long managerID) {
 		this.managerID = managerID;
 	}
-	public long getDepartmentID() {
-		return departmentID;
-	}
-	public void setDepartmentID(long departmentID) {
-		this.departmentID = departmentID;
-	}
-		
-	public long getPassportID() {
-		return passportID;
+
+	public Department getDepartment() {
+		return department;
 	}
 
-	public void setPassportID(long passportID) {
-		this.passportID = passportID;
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Passport getPassport() {
+		return passport;
+	}
+
+	public void setPassport(Passport passport) {
+		this.passport = passport;
 	}
 
 	public String getJobTittle() {
 		return jobTittle;
 	}
+
 	public void setJobTittle(String jobTittle) {
 		this.jobTittle = jobTittle;
 	}
+
 	public int getSalary() {
 		return salary;
 	}
+
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
---
+++ src/main/java/com/enterprise/organization/entities/User.java
@@ -0,0 +1,33 @@
+package com.enterprise.organization.entities;
+
+public class User {
+
+	private long userID;
+	private String login;
+	private String password;
+
+	public long getUserID() {
+		return userID;
+	}
+
+	public void setUserID(long userID) {
+		this.userID = userID;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
